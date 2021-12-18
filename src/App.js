0bfb59f1bc2b33551635a8455f3bd1d789459068
dorj222 <dorj222@gmail.com>
2021-12-18 15:41:25 +0100

    more information button is fully implemented and now fully functional

--- src/App.js
+++ src/App.js
@@ -44,10 +44,9 @@ class App extends Component{
         authors: response.results[0].authors[0].name,
         subject: "",
         genre: "",
-        background: this.generateBackground()
+        background: this.generateBackground(),
       })
-    }, 
-      (error) =>{
+    },  (error) =>{
         this.setState({
           isLoaded: true,
           error
@@ -55,25 +54,19 @@ class App extends Component{
       })
   }
 
-
-
   handleClick() {
     this.callAPI();
-    // console.log("handleClickDetail: ", String(this.isBtnMoreClicked));
-    console.log("handleClickDetail: ", this.isBtnMoreClicked);
     this.setState({
       subject: "",
       genre: "",
       titleWithAuthor: "",
       isBtnMoreClicked: false
     })
-    
   }
 
   handleClickDetail(){
 
-    // console.log("handleClickDetail: ", String(this.isBtnMoreClicked));
-    // if(this.isBtnMoreClicked === false){
+    if(!this.state.isBtnMoreClicked){
     this.setState({
       title: null,
       authors: null,
@@ -83,19 +76,18 @@ class App extends Component{
       genre: "Genre: " + this.state.books.bookshelves[0],
       isBtnMoreClicked: true
         }
-      )
-    // }else{
-    //   this.setState({
-    //     title: this.state.books.title,
-    //     authors: this.state.books.authors[0].name,
-    //     background: this.state.background,
-    //     titleWithAuthor: "www",
-    //     subject: "",
-    //     genre: "",
-    //     isBtnMoreClicked: false
-    //       }
-    //     )
-    // }
+      )}  
+    else{
+      this.setState({
+        title: this.state.books.title,
+        authors: this.state.books.authors[0].name,
+        background: this.state.background,
+        titleWithAuthor: "",
+        subject: "",
+        genre: "",
+        isBtnMoreClicked: false
+          }
+        )}
   }
 
   generateBackground() {
@@ -110,7 +102,7 @@ class App extends Component{
   render(){
     // const title = this.state.books.title;
     const books = this.state.books;
-    console.log('books render: ', books);
+    // console.log('books render: ', books);
     const authors = this.state.authors;
     // console.log('books authors: ', authors);
     const title = this.state.title;
@@ -119,10 +111,10 @@ class App extends Component{
     const genre = this.state.genre;
     const titleWithAuthor = this.state.titleWithAuthor;
     const background = this.state.background;
-    // let background = this.generateBackground();
 
     return (
       <div className="App">
+
             <Book title={title} 
             authors={authors} 
             books={books} 
@@ -130,24 +122,19 @@ class App extends Component{
             genre={genre}
             titleWithAuthor={titleWithAuthor}
             background={background}
+            onClick={() => this.handleClickDetail()}
             />
-            {/* <Detail title={title} authors={authors} books={books}/> */}
+
             <div className='btnContainer'>
-                <button id="btnAbout" onClick={() => 
-                  this.handleClickDetail()
-                  // <Detail title={title} authors={authors} books={books}/>
-                }>
-                    
+                <button id="btnAbout" onClick={() => this.handleClickDetail()}>
                     more <FontAwesomeIcon icon={faInfoCircle}></FontAwesomeIcon>
                 </button>
 
                 <button id="btnBookMark">
-                    
                     save <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
                 </button>
 
-                <button id="btnNext" onClick={() => 
-                  this.handleClick()}>
+                <button id="btnNext" onClick={() => this.handleClick()}>
                     next <FontAwesomeIcon icon={faForward}></FontAwesomeIcon>
                 </button>
             </div>
