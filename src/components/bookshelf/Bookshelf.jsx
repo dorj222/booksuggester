4cb620b5d7f274a8e062ea6da4b91d0383592db0
dorj222 <dorj222@gmail.com>
2024-03-07 10:16:19 +0100

    Update Bookshelf UI

--- src/components/bookshelf/Bookshelf.jsx
+++ src/components/bookshelf/Bookshelf.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './bookshelf.styles.css';
 import { Book } from '../book/Book';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { firestore } from "../../firebase/firebase.utils";
 import EmptyIcon from "../../assets/svg/Empty";
 import styled from "styled-components";
+import StarRatingComponent from 'react-star-rating-component';
+
 
 class Bookshelf extends React.Component {
     constructor() {
@@ -19,7 +20,11 @@ class Bookshelf extends React.Component {
     async componentDidMount() {
         try {
             const snapshot = await firestore.collection("users").doc(this.props.currentUser.id).collection('books').get();
-            const dataBooks = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            const dataBooks = snapshot.docs.map(doc => {
+                const data = doc.data();
+                data.hasRead = data.hasRead || 0; // add this line.
+                return { id: doc.id, ...data };
+            });
             this.setState({ books: dataBooks });
         } catch (error) {
             console.error("Error fetching books:", error);
@@ -41,22 +46,21 @@ class Bookshelf extends React.Component {
         this.componentDidMount()
     }
 
-    handleClickUpdateRead(bookID, hasRead) {
-        const response = async () => {
-            await (firestore.collection("users").doc(this.props.currentUser.id).collection("books").doc(bookID).update({
-                "hasRead": !hasRead
-            })
-                .then(res => {
-                    console.log(res);
-                })
-                .catch((error) => {
-                    console.error("Error updating a document: ", error);
-                })
+    handleClickUpdateRead = async (bookID, rating) => {
+        try {
+            await firestore.collection("users").doc(this.props.currentUser.id).collection("books").doc(bookID).update({
+                "hasRead": rating
+            });
+
+            const books = this.state.books.map(book =>
+                book.id === bookID ? { ...book, hasRead: rating } : book
             );
+
+            this.setState({ books });
+        } catch (error) {
+            console.error("Error updating a document: ", error);
         }
-        response();
-        this.componentDidMount()
-    }
+    };
 
     render() {
         const { books } = this.state;
@@ -74,12 +78,21 @@ class Bookshelf extends React.Component {
                                 genre={book.genre}
                                 background={book.background}
                             />
-                            <button id="btnDelete" onClick={() => this.handleClickDelete(book.id)}>
-                                <FontAwesomeIcon icon={faTrash} /> delete
-                            </button>
-                            <label>
-                                <FontAwesomeIcon icon={faBookmark} />  <span className="checkmark">read </span> <input type="checkbox" checked={book.hasRead} onChange={() => this.handleClickUpdateRead(book.id, book.hasRead)} />
-                            </label>
+                            <WrapperStarRating>
+                                <HoverableStarRating>
+                                    <StarRatingComponent className="starRating"
+                                        name={"rate" + book.id}
+                                        starCount={5}
+                                        value={book.hasRead}
+                                        starColor="#ffd700" 
+                                        emptyStarColor={"grey"}
+                                        onStarClick={(nextValue) => this.handleClickUpdateRead(book.id, nextValue)}
+                                    />
+                                </HoverableStarRating>
+                                <DeleteIcon icon={faTrash} onClick={() => this.handleClickDelete(book.id)}/>
+                            </WrapperStarRating>
+                            
+                            
                         </div>
                     ))
                 ) : (
@@ -98,12 +111,12 @@ export { Bookshelf };
 
 
 const WrapperCardList = styled.nav`
-    width: 75vw;
+    width: 55vw;
     margin: 100px auto 60px;
     display: flex;
     flex-wrap: wrap;
-    justify-content: start; 
-    gap: 20px;
+    justify-content: center; 
+    gap: 40px;
     min-height: 70vh;
 `;
 
@@ -112,4 +125,34 @@ const WrapperMessage = styled.nav`
     justify-content: center;
     align-items: center;
     width: 100%; 
+`;
+
+const WrapperStarRating = styled.nav`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10px;
+    width: 100%; 
+    gap: 20px;
+`;
+
+const DeleteIcon = styled(FontAwesomeIcon)`
+    cursor: pointer;
+    transition: color 0.3s;
+    &:hover {
+        color: #ff0000;
+        opacity: 0.7;
+    }
+`;
+
+const HoverableStarRating = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: opacity 0.3s;
+    color: white;
+    &:hover {
+        opacity: 0.7;
+    }
 `;
